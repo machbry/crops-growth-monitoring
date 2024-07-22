bf61a5bce5707c89622519eb69e5c1031ab62219
machbry <matthieu.chambery@gmail.com>
2024-07-22 23:42:04 +0200

    add requests to db for pipeline to compute index parcels

--- cgm/database/requests.py
+++ cgm/database/requests.py
@@ -1,8 +1,9 @@
-from typing import List
+from typing import List, Tuple
+from uuid import UUID
 
-from sqlalchemy import select
+from sqlalchemy import select, func
 
-from cgm.database.models import Parcel
+from cgm.database.models import Parcel, ParcelQuery, CatalogQuery
 from cgm.database.session import get_session
 
 
@@ -12,3 +13,31 @@ def get_all_rpg_parcels() -> List[Parcel]:
         results = session.execute(query).scalars().all()
 
     return results
+
+
+def get_pending_parcels_for_index_computation_by_catalog_query() -> List[Tuple[UUID, List[int]]]:
+    with get_session() as session:
+        query = select(ParcelQuery.catalog_query_uuid_fk,
+                       func.array_agg(ParcelQuery.parcel_id_fk)
+                       ).filter(ParcelQuery.index_computed_at == None
+        ).group_by(ParcelQuery.catalog_query_uuid_fk)
+
+        results = session.execute(query).all()
+
+    return results
+
+
+def get_catalog_query_by_uuid(catalog_query: UUID) -> CatalogQuery:
+    with get_session() as session:
+        query = select(CatalogQuery).filter(CatalogQuery.uuid == catalog_query)
+        result = session.execute(query).scalar()
+
+    return result
+
+
+def get_parcel_by_id(parcel_id: int) -> Parcel:
+    with get_session() as session:
+        query = select(Parcel).filter(Parcel.id == parcel_id)
+        result = session.execute(query).scalar()
+
+    return result
