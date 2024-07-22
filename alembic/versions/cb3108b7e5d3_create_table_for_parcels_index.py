49522b63c368b3074ff6d3a51d49de6b796020a0
machbry <matthieu.chambery@gmail.com>
2024-07-22 23:41:12 +0200

    add table for parcels index

---
+++ alembic/versions/cb3108b7e5d3_create_table_for_parcels_index.py
@@ -0,0 +1,39 @@
+"""Create table for parcels index
+
+Revision ID: cb3108b7e5d3
+Revises: 7ec9b5033fe3
+Create Date: 2024-07-22 19:09:47.279864
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'cb3108b7e5d3'
+down_revision: Union[str, None] = '7ec9b5033fe3'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.add_column('parcels_queries', sa.Column('index_computed_at', sa.DateTime(timezone=True), nullable=True))
+
+    op.create_table('parcels_index',
+        sa.Column('parcel_id_fk', sa.Integer(), sa.ForeignKey('parcels.id', ondelete="CASCADE"),
+                  nullable=False, primary_key=True, index=True),
+        sa.Column('datetime', sa.DateTime(timezone=True), primary_key=True, index=True, nullable=False),
+        sa.Column('mean_ndvi', sa.Float(), nullable=False),
+        sa.Column('mean_ndmi', sa.Float(), nullable=False),
+        sa.Column('ndvi_cog_file', sa.String(), nullable=False),
+        sa.Column('ndmi_cog_file', sa.String(), nullable=False),
+        sa.ForeignKeyConstraint(['parcel_id_fk'], ['parcels.id'], ondelete='CASCADE')
+    )
+
+
+def downgrade():
+    op.drop_table('parcels_index')
+
+    op.drop_column('parcels_queries', 'index_computed_at')
--- cgm/database/models.py
+++ cgm/database/models.py
@@ -1,6 +1,8 @@
+from ast import Index
+
 from geoalchemy2 import Geometry
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey, Uuid
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey, Uuid, Float, Boolean
 from sqlalchemy.orm import relationship
 
 from cgm.constants import SRID
@@ -17,6 +19,7 @@ class Parcel(Base):
     updated_at = Column("updated_at", DateTime(timezone=True), onupdate=func.now())
 
     parcel_queries = relationship("ParcelQuery", back_populates="parcel", cascade="all, delete")
+    parcel_indexes = relationship("ParcelIndex", back_populates="parcel", cascade="all, delete")
 
 
 class CatalogQuery(Base):
@@ -38,6 +41,20 @@ class ParcelQuery(Base):
     id = Column("id", Integer, primary_key=True, autoincrement=True)
     parcel_id_fk = Column("parcel_id_fk", Integer, ForeignKey('parcels.id', ondelete="CASCADE"), nullable=False)
     catalog_query_uuid_fk = Column("catalog_query_uuid_fk", Uuid, ForeignKey('catalog_queries.uuid', ondelete="CASCADE"), nullable=False)
+    index_computed_at = Column("index_computed_at", DateTime(timezone=True), default=None)
 
     parcel = relationship("Parcel", back_populates="parcel_queries")
     catalog_query = relationship("CatalogQuery", back_populates="parcel_queries")
+
+
+class ParcelIndex(Base):
+    __tablename__ = 'parcels_index'
+    parcel_id_fk = Column("parcel_id_fk", Integer, ForeignKey('parcels.id', ondelete="CASCADE"),
+                          primary_key=True, index=True, nullable=False)
+    datetime = Column("datetime", DateTime(timezone=True), primary_key=True, index=True, nullable=False)
+    mean_ndvi = Column("mean_ndvi", Float, nullable=False)
+    mean_ndmi = Column("mean_ndmi", Float, nullable=False)
+    ndvi_cog_file = Column("ndvi_cog_file", String, nullable=False)
+    ndmi_cog_file = Column("ndmi_cog_file", String, nullable=False)
+
+    parcel = relationship("Parcel", back_populates="parcel_indexes")
