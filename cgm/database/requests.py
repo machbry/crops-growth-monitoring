47adecb242d22d300f5335c11b4a2bb5f8c2d46d
machbry <matthieu.chambery@gmail.com>
2024-07-23 19:18:10 +0200

    add requests to save parcel_index and update parcel_query

--- cgm/database/requests.py
+++ cgm/database/requests.py
@@ -1,7 +1,8 @@
 from typing import List, Tuple
 from uuid import UUID
+from datetime import datetime
 
-from sqlalchemy import select, func
+from sqlalchemy import select, func, and_, update
 
 from cgm.database.models import Parcel, ParcelQuery, CatalogQuery
 from cgm.database.session import get_session
@@ -27,9 +28,9 @@ def get_pending_parcels_for_index_computation_by_catalog_query() -> List[Tuple[U
     return results
 
 
-def get_catalog_query_by_uuid(catalog_query: UUID) -> CatalogQuery:
+def get_catalog_query_by_uuid(catalog_query_uuid: UUID) -> CatalogQuery:
     with get_session() as session:
-        query = select(CatalogQuery).filter(CatalogQuery.uuid == catalog_query)
+        query = select(CatalogQuery).filter(CatalogQuery.uuid == catalog_query_uuid)
         result = session.execute(query).scalar()
 
     return result
@@ -41,3 +42,21 @@ def get_parcel_by_id(parcel_id: int) -> Parcel:
         result = session.execute(query).scalar()
 
     return result
+
+
+def save_parcel_index(parcel: Parcel) -> None:
+    with get_session() as session:
+        session.merge(parcel)
+        session.commit()
+
+
+def update_parcel_query(parcel: Parcel, catalog_query: CatalogQuery, index_computed_at: datetime.timetz) -> None:
+    with get_session() as session:
+        query = update(ParcelQuery).where(
+            and_(
+                ParcelQuery.parcel_id_fk == parcel.id,
+                ParcelQuery.catalog_query_uuid_fk == catalog_query.uuid)
+        ).values(index_computed_at=index_computed_at)
+
+        session.execute(query)
+        session.commit()
